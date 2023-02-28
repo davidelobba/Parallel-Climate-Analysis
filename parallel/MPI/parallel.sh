d04b61923e1dceafcfa46c81a6da3a1b6ec26369
Laiti <francesco.laiti@hpc-head-n1.unitn.it>
2023-02-28 15:20:09 +0100

    add parallel MPI

---
+++ parallel/MPI/parallel.sh
@@ -0,0 +1,24 @@
+#! /bin/bash
+
+#parameters
+#PBS -l select=2:ncpus=2:mem=1gb
+
+# set max exec time
+#PBS -l walltime=0:05:00
+
+# set queue
+#PBS -q short_cpuQ
+
+# load modules
+module load mpich-3.2 hdf5-1.10.5--gcc-9.1.0 netcdf-4.7.0--gcc-9.1.0
+
+# move to the correct folder
+cd HPC_Project/MPI/parallel
+
+# make file
+make
+
+# run the binary file
+mpiexec -n 7 ./parallel
+# mpiexec -n 1 valgrind --leak-check=yes ./parallel # https://stackoverflow.com/questions/34851643/using-valgrind-to-spot-error-in-mpi-code
+# mpiexec -n 1 valgrind --leak-check=yes --track-origins=yes ./parallel # https://stackoverflow.com/questions/34851643/using-valgrind-to-spot-error-in-mpi-code
